ddd87b223368b5a7cd03d9fd72ebfb0ffd335e4b
Praneeth Bollina <51351475+Pran-b@users.noreply.github.com>
2020-11-12 19:53:58 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo "This will go into the body of the mail." | mail -s "Hello world" praneeth07ie001@gmail.com"
+sh -c "echo This will go into the body of the mail. | mail -s "Hello world" praneeth07ie001@gmail.com"
